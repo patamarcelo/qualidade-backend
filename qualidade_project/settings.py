77d910b48a9745f75a3e1358365a5cee2e956798
Marcelo Pata <patamarcelo@gmail.com>
2023-04-01 17:41:22 -0300

    csrf token auth

--- qualidade_project/settings.py
+++ qualidade_project/settings.py
@@ -31,8 +31,8 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*", "https://diamante-quality.up.railway.app/"]
-CSRF_TRUSTED_ORIGINS = ["https://diamante-quality.up.railway.app/"]
+ALLOWED_HOSTS = ["*", "https://diamante-quality.up.railway.app"]
+CSRF_TRUSTED_ORIGINS = ["https://diamante-quality.up.railway.app"]
 
 
 # Application definition
