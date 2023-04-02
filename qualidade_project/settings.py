b58f82e76850ca9155ed82ba4c86ed672bae76ce
Marcelo Pata <patamarcelo@gmail.com>
2023-04-02 10:16:43 -0300

    whitenoise settings

--- qualidade_project/settings.py
+++ qualidade_project/settings.py
@@ -61,6 +61,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -173,3 +174,5 @@ REST_AUTH_SERIALIZERS = {
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
