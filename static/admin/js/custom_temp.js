4cc69105a7f3da0d90c8ffb5ff74d87f9b426fe6
patamarcelo <patamarcelo@gmail.com>
2023-08-07 08:47:04 -0300

    added Vuejs on django Template

--- static/admin/js/custom_temp.js
+++ static/admin/js/custom_temp.js
@@ -2,3 +2,18 @@ console.log("Plantio");
 console.log(plantio);
 console.log("Colheita");
 console.log(colheita);
+
+var app = new Vue({
+	delimiters: ["[[", "]]"],
+	el: "#app",
+	data: {
+		message: "Hello Vue!",
+		plantio: plantio,
+		colheita: colheita
+	},
+	methods: {
+		greet: function (name) {
+			console.log("Hello from " + name + "!");
+		}
+	}
+});
