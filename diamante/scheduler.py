ef254159ee6c9df9a77e4370b7116ccbea4092d8
patamarcelo <patamarcelo@gmail.com>
2024-06-16 22:45:48 -0300

    start a real cron job after 15 minuts on weekday

--- diamante/scheduler.py
+++ diamante/scheduler.py
@@ -21,39 +21,40 @@ def delete_old_job_executions(max_age=604_800):
 
 def start():
     try:
-        module_name, func_name = 'diamante.cron.get_hour_test'.rsplit('.', 1)
-        module = import_module(module_name)
-        func = getattr(module, func_name)
-        print(f"Funcao encontrada: {func}")
         scheduler = BackgroundScheduler()
-        scheduler.add_jobstore(DjangoJobStore(), "default")
-
-        if settings.DEBUG == True:
+        # remove all runing jobs
+        scheduler.remove_all_jobs()
+        if settings.DEBUG == False:
+            module_name, func_name = 'diamante.cron.get_hour_test'.rsplit('.', 1)
+            module = import_module(module_name)
+            func = getattr(module, func_name)
+            print(f"Funcao encontrada: {func}")
+            scheduler.add_jobstore(DjangoJobStore(), "default")
             print('agendando funcao para rodar no servidor:')
             # Register the job with a textual reference
             scheduler.add_job(
                 func,
                 'cron',
                 day_of_week="*",
-                hour="*",
-                minute="*",
-                id="Imprimindo a cada segundo no servidor LOCAL"
+                hour="6-19",  # From 6 AM to 7:59 PM
+                minute="0,15,30,45",  # At 0, 15, 30, and 45 minutes of each hour
+                id="job_every_15_minutes__last__try"
             )
+            register_events(scheduler)
+            scheduler.start()
+            logger.info("Scheduler started!")
+
+            # Cleanup old job executions
+            scheduler.add_job(
+                delete_old_job_executions,
+                trigger='interval',
+                days=7,
+                id='delete_old_job_executions',
+                max_instances=1,
+                replace_existing=True,
+            )
+            logger.info("Added job: 'delete_old_job_executions'.")
         else:
             print('funcionando, vai rodar somente no servidor')
-        register_events(scheduler)
-        scheduler.start()
-        logger.info("Scheduler started!")
-
-        # Cleanup old job executions
-        scheduler.add_job(
-            delete_old_job_executions,
-            trigger='interval',
-            days=7,
-            id='delete_old_job_executions',
-            max_instances=1,
-            replace_existing=True,
-        )
-        logger.info("Added job: 'delete_old_job_executions'.")
     except Exception as e:
         print(f"Error resolving function: {e}")
